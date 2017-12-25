d87c5b3ce9b2dd4581b9e013410c3ce82a0f382b
liuyj-m <liuyj-m@glodon.com>
2017-12-25 18:04:21 +0800

    AAAA

--- src/a01_FactoryMethod/MailSender.java
+++ src/a01_FactoryMethod/MailSender.java
@@ -4,7 +4,6 @@ public class MailSender implements Sender {
     @Override  
     public void Send() {  
         System.out.println("this is mailsender!");  
-//        test
-        //232432432
+        //111
     }  
 }  
--- src/a01_FactoryMethod/Sender.java
+++ src/a01_FactoryMethod/Sender.java
@@ -2,6 +2,5 @@ package a01_FactoryMethod;
 
 public interface Sender {  
     public void Send();  
-    //t2
-    //12321321
+    //22
 }  
