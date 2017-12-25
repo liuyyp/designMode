5c7576602644a9c44c6bd0e42231e5ee7cce8ddd
liuyj-m <liuyj-m@glodon.com>
2017-12-25 10:40:01 +0800

    啊啊啊

--- src/a01_FactoryMethod/Test.java
+++ src/a01_FactoryMethod/Test.java
@@ -1,24 +1,15 @@
 package a01_FactoryMethod;
 
 /**
- * 普通工厂模式 ：就是建立一个工厂类，对实现了同一接口的一些类进行实例的创建
- * 
+ * 普通工厂模式  ：就是建立一个工厂类，对实现了同一接口的一些类进行实例的创建
  * @author CC
  *
  */
 public class Test {
 
-	public static void main(String[] args) {
-		int sum = 0;
-		outer: for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				for (int k = 0; k < 10; k++) {
-					sum++;
-					if (sum > 500) {
-						break outer;
-					}
-				}
-			}
-		}
-	}
+	public static void main(String[] args) {  
+        SendFactory factory = new SendFactory();  
+        Sender sender = factory.produce("sms");  
+        sender.Send();  
+    }
 }
